ab3e1388e33c5fe66d5be7d796bddec474969e7b
ARharrhour <abderrahmanerharrhour@gmail.com>
2023-11-13 23:21:51 +0100

    first commit vueshop

---
+++ src/store/index.js
@@ -0,0 +1,56 @@
+import { createStore } from "vuex";
+import cart from "./modules/cart.js"
+import product from "./modules/product.js"
+const store = createStore({
+  modules: {
+    cart: cart,
+    product: product
+  },
+  state() {
+    return {
+      isLoggedIn: false,
+
+
+
+
+    }
+  },
+  mutations: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+
+  },
+  actions: {
+    login(context) {
+      context.commit("login")
+    },
+    logout(context) {
+      context.commit("logout")
+    }
+
+  },
+
+  getters: {
+
+    isAuthenticated(state) {
+      return state.isLoggedIn;
+    }
+
+
+  },
+
+
+
+
+
+
+
+
+
+
+});
+export default store;
---
+++ src/store/modules/cart.js
@@ -0,0 +1,79 @@
+export default {
+  namespaced: true,
+  state() {
+    return {
+      items: [],
+      total: 0,
+      qty: 0
+    }
+
+  },
+  mutations: {
+    addProductToCart(state, playload) {
+      const productData = playload
+      const productInCartIndex = state.items.findIndex(
+        (ci) => ci.productId === productData.id
+      );
+
+      if (productInCartIndex >= 0) {
+        state.items[productInCartIndex].qty++;
+      } else {
+        const newItem = {
+          productId: productData.id,
+          title: productData.title,
+          image: productData.image,
+          price: productData.price,
+          qty: 1,
+        };
+        state.items.push(newItem);
+      }
+      state.qty++;
+      state.total += productData.price;
+
+    }
+    ,
+    removeProductFromCart(state, playload) {
+      const prodId = playload.productId
+      const productInCartIndex = state.items.findIndex(
+        (cartItem) => cartItem.productId === prodId
+      );
+      const prodData = state.items[productInCartIndex];
+      state.items.splice(productInCartIndex, 1);
+      state.qty -= prodData.qty;
+      state.total -= prodData.price * prodData.qty;
+    }
+  },
+  actions: {
+
+    addToCart(context, payload) {
+      const prodId = payload.id;
+      const products = context.rootGetters['product/products'];
+      const product = products.find(prod => prod.id === prodId);
+      context.commit('addProductToCart', product);
+    },
+    removeFromCart(context, payload) {
+      context.commit('removeProductFromCart', payload);
+    }
+
+
+  },
+  getters: {
+
+    products(state) {
+      return state.items;
+    },
+    totalSum(state) {
+      return state.total.toFixed(2);
+    },
+    quantity(state) {
+      return state.qty;
+    }
+
+  }
+
+
+
+
+
+
+}
